12e9a1f2abc2f19b05734d42a1a38712288b00f8
abhiptl <abhishek.patel@sanelib.com>
2017-03-15 15:52:11 +0530

    adding activity to get capital details - work in progress

---
+++ app/src/main/java/gov/com/esurvey/CapitalDetailActivity.java
@@ -0,0 +1,13 @@
+package gov.com.esurvey;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CapitalDetailActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_capital_detaial);
+	}
+}
